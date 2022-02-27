a5d03f8d5663a3855d58116ca8058b884542497c
Hábele Márton András <marton.habele@gmail.com>
2022-02-27 22:32:13 +0100

    Day01 tasks updated

--- src/main/java/day01/Main.java
+++ src/main/java/day01/Main.java
@@ -1,22 +1,25 @@
 package day01;
 
+import org.flywaydb.core.Flyway;
 import org.mariadb.jdbc.MariaDbDataSource;
-import java.sql.Connection;
 import java.sql.SQLException;
-import java.sql.Statement;
 
 public class Main {
     public static void main(String[] args) {
         MariaDbDataSource dataSource = new MariaDbDataSource();
         try {
-            dataSource.setUrl("jdbc:mariadb://localhost:3306/movies-actors");
+            dataSource.setUrl("jdbc:mariadb://localhost:3306/movies-actors?useUnicode=true");
             dataSource.setUserName("root");
             dataSource.setPassword("root");
         } catch (SQLException e) {
             throw new IllegalStateException("Database cannot be reached!");
         }
 
+        Flyway flyway = Flyway.configure().dataSource(dataSource).load();
+        flyway.migrate();
+
         ActorsRepository actorsRepository = new ActorsRepository(dataSource);
-        actorsRepository.saveActor("Jack Doe");
+        actorsRepository.saveActor("John Doe");
+        System.out.println(actorsRepository.findActorsWithPrefix("j"));
     }
 }
---
+++ src/main/resources/db/migration/V2__movies.sql
@@ -0,0 +1,5 @@
+CREATE TABLE movies (
+id BIGINT AUTO_INCREMENT,
+title VARCHAR(255),
+release_date DATE,
+CONSTRAINT pk_movies PRIMARY KEY (id));
